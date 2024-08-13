9f5087e1dbcf7d09daf6b64989181aa1370e8cfa
Pereverzev Marat <marat.pereverzrv@gmail.com>
2024-08-13 12:07:09 +0300

    init

---
+++ client/craco.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const fs = require("fs");
+const evalSourceMap = require("react-dev-utils/evalSourceMapMiddleware");
+const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+const noopServiceWorker = require("react-dev-utils/noopServiceWorkerMiddleware");
+
+const resolvePath = (p) => path.resolve(__dirname, p);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": resolvePath("./src/components"),
+      "@context": resolvePath("./src/context"),
+      "@utils": resolvePath("./src/utils"),
+      "@pages": resolvePath("./src/pages"),
+    },
+  },
+  eslint: false,
+  devServer: (devServerConfig, { env, paths }) => {
+    devServerConfig = {
+      ...devServerConfig,
+      onBeforeSetupMiddleware: undefined,
+      onAfterSetupMiddleware: undefined,
+      setupMiddlewares: (middleware, devServer) => {
+        if (!devServer) {
+          throw new Error("webpack-dev-server is not defined");
+        }
+
+        if (fs.existsSync(paths.proxySetup)) {
+          require(paths.proxySetup)(devServer.app);
+        }
+
+        middleware.push(
+          evalSourceMap(devServer),
+          redirectServedPath(paths.publicUrlOrPath),
+          noopServiceWorker(paths.publicUrlOrPath)
+        );
+
+        return middleware;
+      },
+    };
+    devServerConfig.client.overlay.warnings = true;
+    return devServerConfig;
+  },
+};
